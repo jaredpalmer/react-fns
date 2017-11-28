a31bc2032526d9370c7b0ef3293b3f302b740b9a
Jeppe Reinhold <jeppereinhold@gmail.com>
2017-11-28 22:41:06 +0100

    reimplement pull #56

--- src/utils/featureDetection.ts
+++ src/utils/featureDetection.ts
@@ -1,13 +1,15 @@
 export let supportsPassiveListener = false;
 
+const noop = () => {};
+
 try {
   const opts = Object.defineProperty({}, 'passive', {
     get: function() {
       supportsPassiveListener = true;
     },
   });
-  (window as EventTarget).addEventListener('testPassive', undefined, opts);
-  (window as EventTarget).removeEventListener('testPassive', undefined, opts);
+  (window as EventTarget).addEventListener('testPassive', noop, opts);
+  (window as EventTarget).removeEventListener('testPassive', noop, opts);
 } catch (e) {}
 
 export const supportsVibrationAPI = navigator && 'vibrate' in navigator;
