99d25b650d32ae4559a182c6a6995b1229029669
Goce Cvetanov <Goce.Cvetanov@netcetera.com>
2018-02-13 21:13:41 +0100

    format render

--- src/utils/__tests__/render.test.tsx
+++ src/utils/__tests__/render.test.tsx
@@ -1,91 +1,88 @@
-
 import * as React from 'react';
 import { render } from '../';
 
 jest.mock('react', () => ({
-    createElement: jest.fn(),
-    Children: {
-        only: jest.fn(),
-        count: jest.fn(() => 1), // mock only child
-    },
+  createElement: jest.fn(),
+  Children: {
+    only: jest.fn(),
+    count: jest.fn(() => 1), // mock only child
+  },
 }));
 
 describe('render()', () => {
+  it('renders the component by using `React.createElement`', () => {
+    // Given
+    const component = jest.fn();
+    const props = {
+      component,
+    };
+    const state = {};
 
+    // When
+    render(props, state);
 
-    it('renders the component by using `React.createElement`', () => {
-        // Given
-        const component = jest.fn();
-        const props = {
-            component,
-        };
-        const state = {};
+    // Then
+    expect(React.createElement).toHaveBeenCalledWith(component, state);
+  });
 
-        // When
-        render(props, state);
+  it('invokes the render prop', () => {
+    // Given
+    const renderProp = jest.fn();
+    const props = {
+      render: renderProp,
+    };
+    const state = {};
 
-        // Then
-        expect(React.createElement).toHaveBeenCalledWith(component, state);
-    });
+    // When
+    render(props, state);
 
-    it('invokes the render prop', () => {
-        // Given
-        const renderProp = jest.fn();
-        const props = {
-            render: renderProp,
-        };
-        const state = {};
+    // Then
+    expect(renderProp).toHaveBeenCalledTimes(1);
+    expect(renderProp).toHaveBeenCalledWith(state);
+  });
 
-        // When
-        render(props, state);
+  it('renders the children as a function', () => {
+    // Given
+    const children = jest.fn();
+    const props = {
+      children,
+    };
+    const state = {};
 
-        // Then
-        expect(renderProp).toHaveBeenCalledTimes(1);
-        expect(renderProp).toHaveBeenCalledWith(state);
-    });
+    // When
+    render(props, state);
 
-    it('renders the children as a function', () => {
-        // Given
-        const children = jest.fn();
-        const props = {
-            children,
-        };
-        const state = {};
+    // Then
+    expect(children).toHaveBeenCalledTimes(1);
+    expect(children).toHaveBeenCalledWith(state);
+  });
 
-        // When
-        render(props, state);
+  it('renders the children by using `React.Children.only`', () => {
+    // Given
+    const DumbComponent = jest.fn(() => <div>dummy</div>);
+    const children = new DumbComponent();
+    const props = {
+      children,
+    };
+    const state = {};
 
-        // Then
-        expect(children).toHaveBeenCalledTimes(1);
-        expect(children).toHaveBeenCalledWith(state);
-    });
+    // When
+    render(props, state);
 
-    it('renders the children by using `React.Children.only`', () => {
-        // Given
-        const DumbComponent = jest.fn(() => <div>dummy</div>);
-        const children = new DumbComponent();
-        const props = {
-            children,
-        };
-        const state = {};
+    // Then
+    expect(React.Children.only).toHaveBeenCalledTimes(1);
+    expect(React.Children.only).toHaveBeenCalledWith(children);
+  });
 
-        // When
-        render(props, state);
+  it('renders `null` otherwise', () => {
+    // Given
+    const props = {};
+    const state = {};
 
-        // Then
-        expect(React.Children.only).toHaveBeenCalledTimes(1);
-        expect(React.Children.only).toHaveBeenCalledWith(children);
-    });
+    // When
+    const result = render(props, state);
 
-    it('renders `null` otherwise', () => {
-        // Given
-        const props = {};
-        const state = {};
-
-        // When
-        const result = render(props, state);
-
-        // Then
-        expect(result).toEqual(null);
-    });
+    // Then
+    expect(result).toEqual(null);
+  });
 });
--- src/utils/render.ts
+++ src/utils/render.ts
@@ -3,20 +3,23 @@ import { isEmptyChildren } from './index';
 import { SharedRenderProps } from '../types';
 
 /**
- * Renders a component (or render prop, or children) by using the following conditioning order:
+ * Renders a component (or render prop/children) by using the following conditioning order:
  * - component
  * - render prop
  * - children as function
  * - children as component
  */
-export function render<P>({ component, render: renderProp, children }: SharedRenderProps<P>, state: P) {
-    return component
-        ? React.createElement(component as any, state)
-        : renderProp
-            ? (renderProp as any)(state)
-            : children
-                ? typeof children === 'function'
-                    ? children(state)
-                    : !isEmptyChildren(children) ? React.Children.only(children) : null
-                : null;
+export function render<P>(
+  { component, render: renderProp, children }: SharedRenderProps<P>,
+  state: P
+) {
+  return component
+    ? React.createElement(component as any, state)
+    : renderProp
+      ? (renderProp as any)(state)
+      : children
+        ? typeof children === 'function'
+          ? children(state)
+          : !isEmptyChildren(children) ? React.Children.only(children) : null
+        : null;
 }
